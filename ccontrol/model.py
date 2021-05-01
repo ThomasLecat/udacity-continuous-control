8fe3164a3ca39148fccff04ab98706a99bb2dd16
Thomas Lecat <t.lecat@instadeep.com>
2021-05-01 14:45:43 +0200

    refactor(model): improve variable names

--- ccontrol/model.py
+++ ccontrol/model.py
@@ -4,43 +4,40 @@ import torch.nn.functional as F
 
 
 class Actor(nn.Module):
-    """Actor Model."""
+    """MultiLayer perceptron. Approximates \argmax_a{Q(s,a)}"""
 
-    def __init__(self, state_size, action_size):
-        """Initialize model."""
+    def __init__(self, state_size, num_actions):
         super().__init__()
         self.fc1 = nn.Linear(state_size, 128)
         self.fc2 = nn.Linear(128, 128)
         self.fc3 = nn.Linear(128, 64)
-        self.fc4 = nn.Linear(64, action_size)
+        self.fc4 = nn.Linear(64, num_actions)
 
-        self.bn1 = nn.BatchNorm1d(128)
+        self.batch_norm = nn.BatchNorm1d(128)
 
-    def forward(self, state):
-        """Forward pass, get action from state."""
-        x = F.relu(self.bn1(self.fc1(state)))
+    def forward(self, observations):
+        x = F.relu(self.batch_norm(self.fc1(observations)))
         x = F.relu(self.fc2(x))
         x = F.relu(self.fc3(x))
         return F.tanh(self.fc4(x))
 
 
 class Critic(nn.Module):
-    """Q-network model."""
+    """Q-network model"""
 
     def __init__(self, state_size, action_size):
-        """Initialize model."""
         super(Critic, self).__init__()
         self.fc1 = nn.Linear(state_size, 128)
         self.fc2 = nn.Linear(128 + action_size, 128)
         self.fc3 = nn.Linear(128, 64)
         self.fc4 = nn.Linear(64, 1)
 
-        self.bn1 = nn.BatchNorm1d(128)
+        self.batch_norm = nn.BatchNorm1d(128)
 
-    def forward(self, observation, action):
-        """Forward pass, get Q value from action and state."""
-        x = F.relu(self.bn1(self.fc1(observation)))
-        x = torch.cat((x, action), dim=1)
+    def forward(self, observations, actions):
+        """Get Q(s,a) for all agents."""
+        x = F.relu(self.batch_norm(self.fc1(observations)))
+        x = torch.cat((x, actions), dim=1)
         x = F.relu(self.fc2(x))
         x = F.relu(self.fc3(x))
         return self.fc4(x)
